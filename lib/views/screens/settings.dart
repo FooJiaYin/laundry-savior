f32a8a1add7a2ee6de313354d7ff26181e4c0bc1
FooJiaYin <candy23198@hotmail.com>
2022-11-07 21:06:11 +0800

    feat: SettingPage titleRow

--- lib/views/screens/settings.dart
+++ lib/views/screens/settings.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../generated/l10n.dart';
+import '../../theme/theme.dart';
+import '../components/setting_item.dart';
+import '../widgets/button.dart';
+import '../widgets/scaffold_page.dart';
+
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
 
@@ -10,6 +16,30 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScaffoldPage(
+      appBar: AppBar(
+        title: _titleRow(),
+        automaticallyImplyLeading: false,
+      ),
+      child: Column(
+        children: [],
+      ),
+    );
   }
+
+  Widget _titleRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          RoundIconButton(
+            Icons.keyboard_arrow_left_rounded,
+            backgroundSize: 40,
+            iconColor: ThemeColors.grey,
+            backgroundColor: ThemeColors.backgroundColor,
+            shadows: ThemeDecoration.neumorphicShadow,
+            onTap: () => Navigator.pop(context),
+          ),
+          Text(S.of(context).settings),
+          const SizedBox(width: 40),
+        ],
+      );
 }
