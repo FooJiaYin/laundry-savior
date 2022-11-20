6bc6246d5cf8d949f9c4c2eb8f91680515a697de
FooJiaYin <candy23198@hotmail.com>
2022-11-21 04:08:48 +0800

    chore: Change WaitingSwitch size

--- lib/views/components/waiting_switch.dart
+++ lib/views/components/waiting_switch.dart
@@ -18,26 +18,26 @@ class WaitingSwitch extends StatelessWidget {
     // if (true) {
     if (state.currentMachine == null) {
       return SizedBox(
-        height: 60,
-        child: FittedBox(
-          fit: BoxFit.fill,
-          child: Switch(
-            thumbColor: MaterialStateProperty.all<Color>(ThemeColors.backgroundColor),
-            activeTrackColor: ThemeColors.primaryColor,
-            inactiveTrackColor: ThemeColors.grey,
-            activeThumbImage: const AssetImage("assets/images/switch_active_thumb.png"),
-            inactiveThumbImage: const AssetImage("assets/images/switch_inactive_thumb.png"),
-            value: state.waitingMachine == machineType && (state.status == Status.waitingFloor || state.status == Status.waitingAll),
-            onChanged: (value) {
-              if (state.waitingMachine != machineType || (state.status != Status.waitingFloor && state.status != Status.waitingAll)) {
-                state.update(status: Status.waitingFloor, waitingMachine: machineType);
-              } else if (state.status != Status.waitingFloor || state.status != Status.waitingAll) {
-                state.update(status: Status.idle);
-              }
-            },
+        height: 48,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Switch(
+              thumbColor: MaterialStateProperty.all<Color>(ThemeColors.backgroundColor),
+              activeTrackColor: ThemeColors.primaryColor,
+              inactiveTrackColor: ThemeColors.grey,
+              activeThumbImage: const AssetImage("assets/images/switch_active_thumb.png"),
+              inactiveThumbImage: const AssetImage("assets/images/switch_inactive_thumb.png"),
+              value: state.waitingMachine == machineType && (state.status == Status.waitingFloor || state.status == Status.waitingAll),
+              onChanged: (value) {
+                if (state.waitingMachine != machineType || (state.status != Status.waitingFloor && state.status != Status.waitingAll)) {
+                  state.update(status: Status.waitingFloor, waitingMachine: machineType);
+                } else if (state.status != Status.waitingFloor || state.status != Status.waitingAll) {
+                  state.update(status: Status.idle);
+                }
+              },
+            ),
           ),
-        ),
       );
-    } else return SizedBox(height: 60);
+    } else return SizedBox(height: 42);
   }
 }
