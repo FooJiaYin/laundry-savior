7b7825327abddca33ca0b793ef71a05436e995f2
FooJiaYin <candy23198@hotmail.com>
2022-12-01 04:59:23 +0800

    feat: Add comfirm exit dialog in PaymentPage

--- lib/views/components/exit_alert_dialog.dart
+++ lib/views/components/exit_alert_dialog.dart
@@ -1,16 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 import '../../theme/theme.dart';
 import 'neumorphic_button.dart';
 
 class ExitAlertDialog extends StatelessWidget {
-  const ExitAlertDialog({Key? key}) : super(key: key);
+  const ExitAlertDialog({
+    Key? key,
+    this.title = "Are you sure to exit?",
+    this.description = "",
+  }) : super(key: key);
+  final String title;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Are you sure to exit?'),
-      content: Text("You have paid for the machine!", style: ThemeFont.style()),
+      title: Text(title),
+      content: Text(description, style: ThemeFont.style()),
       actions: <Widget>[
         NeumorphicButton(text: "Exit", textColor: ThemeColors.coral, width: 50, onPressed: () => Navigator.of(context).pop(true)),
         NeumorphicButton(text: "Cancel", width: 50, onPressed: () => Navigator.of(context).pop(false)),
--- lib/views/screens/payment.dart
+++ lib/views/screens/payment.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../../models/global_state.dart';
+import '../components/exit_alert_dialog.dart';
 import '../widgets/scaffold_page.dart';
 
 class PaymentPage extends StatefulWidget {
@@ -13,14 +15,23 @@ class PaymentPage extends StatefulWidget {
 class _PaymentPageState extends State<PaymentPage> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, "/current_machine"),
-      child: const ScaffoldPage(
-        backgroundColor: Colors.black,
-        backgroundImage: DecorationImage(
-          image: AssetImage("assets/images/payment.png"),
-          fit: BoxFit.contain,
-          alignment: Alignment.bottomCenter,
+    return WillPopScope(
+      onWillPop: () async {
+        final willPop = await showDialog<bool>(context: context, builder: (context) => ExitAlertDialog(title: "Payment not completed", description: "Are you sure to exit?"));
+        return willPop!;
+      },
+      child: GestureDetector(
+        onTap: () {
+          GlobalState.set(context, status: Status.mode);
+          Navigator.pushNamed(context, "/current_machine");
+        },
+        child: const ScaffoldPage(
+          backgroundColor: Colors.black,
+          backgroundImage: DecorationImage(
+            image: AssetImage("assets/images/payment.png"),
+            fit: BoxFit.contain,
+            alignment: Alignment.bottomCenter,
+          ),
         ),
       ),
     );
